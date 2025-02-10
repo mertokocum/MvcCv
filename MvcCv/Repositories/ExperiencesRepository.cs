e1bb101a459e830733fe9b62acad3bb2701c7556
mertokocum <hmrtkocum@gmail.com>
2025-02-10 22:51:26 +0300

    24. Repository Design Pattern

---
+++ MvcCv/Repositories/ExperiencesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Repositories
+{
+    public class ExperiencesRepository
+    {
+        ////DbCvEntities db = new DbCvEntities();
+        ////public List<TblExperiences> List()
+        ////{
+        ////    return db.TblExperiences.ToList();
+        ////}
+    }
+}
