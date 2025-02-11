aafc3d5f7909c2fc7ab258229c5855cb7b902607
mertokocum <hmrtkocum@gmail.com>
2025-02-11 03:53:03 +0300

    33. Yetenekleri Repository Üzerinden Listeleme

--- MvcCv/Controllers/SkillsController.cs
+++ MvcCv/Controllers/SkillsController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
 
 namespace MvcCv.Controllers
 {
     public class SkillsController : Controller
     {
         // GET: Skills
+        GenericRepository<TblSkills> repo = new GenericRepository<TblSkills>();
         public ActionResult Index()
         {
-            return View();
+            var skills=repo.List();
+            return View(skills);
         }
     }
 }
