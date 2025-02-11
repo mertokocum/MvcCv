de668a26ed5148d697fd1bba61a98527b7147d05
mertokocum <hmrtkocum@gmail.com>
2025-02-12 02:01:23 +0300

    42.AddEducation

---
+++ MvcCv/Controllers/EducationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+
+namespace MvcCv.Controllers
+{
+    public class EducationController : Controller
+    {
+        // GET: Education
+        GenericRepository<TblEducation> repo = new GenericRepository<TblEducation>();
+
+        public ActionResult Index()
+        {
+            var education = repo.List();
+
+            return View(education);
+        }
+        [HttpGet]
+        public ActionResult AddEducation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEducation(TblEducation p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
