c4352171843e8499dac8abef4ddedfe8b9c2ac52
mertokocum <hmrtkocum@gmail.com>
2025-02-14 02:34:56 +0300

    48. Sertifika bilgilerinin listelenmesi.

--- MvcCv/Controllers/CertificateController.cs
+++ MvcCv/Controllers/CertificateController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
 
 namespace MvcCv.Controllers
 {
     public class CertificateController : Controller
     {
         // GET: Certificate
+        GenericRepository<TblSertificates> repo = new GenericRepository<TblSertificates>();
         public ActionResult Index()
         {
-            return View();
+            var certificate = repo.List();
+            return View(certificate);
         }
     }
 }
