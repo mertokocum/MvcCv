a5f1dc3bc3c1f101feb8198860e0a428add4b646
mertokocum <hmrtkocum@gmail.com>
2025-03-04 23:42:45 +0300

    52. iletisim paneli

---
+++ MvcCv/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+
+namespace MvcCv.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        GenericRepository<TblMessage> repo = new GenericRepository<TblMessage>();
+        public ActionResult Index()
+        {
+            var messages = repo.List();
+            return View(messages);
+        }
+    }
+}
